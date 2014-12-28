14ffe3ae93303161d5c4c688055d2c41cbbda092
Axel Huizinga <noreply>
2014-12-28 22:02:48 +0000

    Add component new-topic

---
+++ ui/new-topic.reel/new-topic.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/new-topic.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class NewTopic
+ * @extends Component
+ */
+exports.NewTopic = Component.specialize(/** @lends NewTopic# */ {
+    constructor: {
+        value: function NewTopic() {
+            this.super();
+        }
+    }
+});
