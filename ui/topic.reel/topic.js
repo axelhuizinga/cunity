5a39f700ff7c7cefd15c94de95b03f6701ae6a2a
Axel Huizinga <noreply>
2014-12-28 22:03:44 +0000

    Add component topic

---
+++ ui/topic.reel/topic.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/topic.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Topic
+ * @extends Component
+ */
+exports.Topic = Component.specialize(/** @lends Topic# */ {
+    constructor: {
+        value: function Topic() {
+            this.super();
+        }
+    }
+});
